ec0c0b7f3b0b44ba75c453a04ec8c643b14a2bbf
Yuliia Hubska <zhukapuka10@gmail.com>
2025-01-31 10:57:12 +0200

    a

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,12 @@
-function getElementWidth(content, padding, border) {
-  return (
-    Number.parseFloat(content) +
-    2 * Number.parseFloat(padding) +
-    2 * Number.parseFloat(border)
-  );
+function checkForSpam(message) {
+  let findSpam = message.toLowerCase();
+  return findSpam.includes("spam") || findSpam.includes("sale");
 }
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+//mentor checkout
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
